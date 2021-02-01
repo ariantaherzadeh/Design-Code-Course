a35e3e33a28e519ce366470a32d6c0d5036deb03
Arian Taherzadeh <arian@taherzadeh.ca>
2021-02-01 16:21:31 -0500

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Arian Taherzadeh on 2021-02-01.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
