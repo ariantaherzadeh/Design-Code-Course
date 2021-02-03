5cb5d2c3ca23486424b742cdf8a59f81815d8530
Arian Taherzadeh <arian@taherzadeh.ca>
2021-02-03 15:40:59 -0500

    Init Commit

---
+++ Shared/CoursesView.swift
@@ -0,0 +1,65 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Arian Taherzadeh on 2021-02-01.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    @State var show = false
+    @Namespace var namespace
+    
+    var body: some View {
+        ZStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(courses) { item in
+                        CourseItem(course: item)
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(width: 335, height: 250)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            if show {
+                ScrollView {
+                    CourseItem(course: courses[0])
+                        .matchedGeometryEffect(id: courses[0].id, in: namespace)
+                        .frame(height: 300)
+                    VStack {
+                        ForEach(0 ..< 20) { item in
+                            CourseRow()
+                                .padding()
+                        }
+                    }
+                    .padding()
+                }
+                .background(Color("Background 1"))
+                .transition(
+                    .asymmetric(
+                        insertion: AnyTransition
+                            .opacity
+                            .animation(Animation.spring().delay(0.3)),
+                        removal: AnyTransition
+                            .opacity
+                            .animation(Animation.spring()))
+                    
+                )
+                .edgesIgnoringSafeArea(.all)
+            }
+        }
+        .onTapGesture {
+            withAnimation(.spring()) {
+                show.toggle()
+            }
+        }
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
